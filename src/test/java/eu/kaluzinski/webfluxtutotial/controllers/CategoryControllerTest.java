cb2cf11a5a821cd3026959662b7a06e088f00d9a
akaluzinski <kaluza@gmail.com>
2020-11-12 16:20:47 +0100

    Add mockito tests of CategoryController

---
+++ src/test/java/eu/kaluzinski/webfluxtutotial/controllers/CategoryControllerTest.java
@@ -0,0 +1,51 @@
+package eu.kaluzinski.webfluxtutotial.controllers;
+
+import eu.kaluzinski.webfluxtutotial.domain.Category;
+import eu.kaluzinski.webfluxtutotial.repository.CategoryRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.BDDMockito;
+import org.mockito.Mockito;
+import org.springframework.test.web.reactive.server.WebTestClient;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+public class CategoryControllerTest {
+
+    WebTestClient webTestClient;
+    CategoryRepository categoryRepository;
+    CategoryController categoryController;
+
+
+    @BeforeEach
+    public void setUp() {
+        categoryRepository = Mockito.mock(CategoryRepository.class);
+        categoryController = new CategoryController(categoryRepository);
+        webTestClient = WebTestClient.bindToController(categoryController).build();
+    }
+
+    @Test
+    public void shouldGetList() {
+        BDDMockito.given(categoryRepository.findAll())
+                .willReturn(Flux.just(Category.builder().description("cat1").build(),
+                        Category.builder().description("cat2").build()));
+
+        webTestClient.get()
+                .uri("/api/v1/categories/")
+                .exchange()
+                .expectBodyList(Category.class)
+                .hasSize(2);
+    }
+
+    @Test
+    public void shouldGetById() {
+        BDDMockito.given(categoryRepository.findById("someid"))
+                .willReturn(Mono.just(Category.builder().description("Cat").build()));
+
+        webTestClient.get()
+                .uri("/api/v1/categories/someid")
+                .exchange()
+                .expectBody(Category.class);
+
+    }
+}
